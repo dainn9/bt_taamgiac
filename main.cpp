293fdd7c5dd3b75465be34af69f26c75ab453d09
Thanh Danh <thanhdanh431@gmail.com>
2023-11-01 20:09:11 +0700

    second commit

--- main.cpp
+++ main.cpp
@@ -22,13 +22,19 @@ class Diem{
         friend Diem operator-(const Diem& A, const Diem& B);
         // tinh khoang cach
         double kCach(const Diem& B);
-        friend double fcvgb(const Diem& A, const Diem& B);
+        friend double fkCach(const Diem& A, const Diem& B);
         // trung diem
         friend Diem ftrungDiem(const Diem& A, const Diem& B);
         Diem Tdiem(const Diem& A);
-        // diem doi xung 
-        Diem doiXung(const Diem& A);
-        friend Diem Doixung(const Diem& A, const Diem& B);
+        // diem doi xung goc toa do O, Ox, Oy
+        Diem doiXung_O();
+        Diem doiXung_Ox();
+        Diem doiXung_Oy();
+
+        friend Diem fdoixung_O(const Diem& A);
+        friend Diem fdoixung_Ox(const Diem& A);
+        friend Diem fdoixung_Oy(const Diem& A);
+
         // Kiem tra trung nhau
         bool KTtrungnhau(const Diem& A);
         // Kiem tra diem thuoc phan tu nao
@@ -61,16 +67,104 @@ double Diem::kCach(const Diem& B){
     return sqrt(pow(x-B.x, 2) + pow(y-B.y, 2));
 }
 
-double Kcach(const Diem& A, const Diem& B)
+double fkCach(const Diem& A, const Diem& B){
+    return sqrt(pow(A.x - B.x, 2) + pow(A.y - B.y,2));
+}
 
-Diem trungDiem(const Diem& A, const Diem& B);
+Diem ftrungDiem(const Diem& A, const Diem& B){
+    Diem C;
+    C.setX((A.x + B.x) / 2);
+    C.setY((A.y + B.y) / 2);
+    return C;
+}
 
-Diem Tdiem(const Diem& A);
-Diem doiXung(const Diem& A);
-Diem Doixung(const Diem& A, const Diem& B)    
-bool KTtrungnhau(const Diem& A){
+Diem Diem::Tdiem(const Diem& A){
+    Diem C;
+    C.setX((x + A.x) / 2);
+    C.setY((y + A.y) / 2);
+    return C;
+}
+Diem Diem::doiXung_O(){
+    Diem C(0-x, 0-y);
+    return C;
+}
+
+Diem Diem::doiXung_Ox(){
+    Diem C(x, 0-y);
+    return C;
+}
+
+Diem Diem::doiXung_Oy(){
+    Diem C(0-x, y);
+    return C;
+}
+
+Diem fDoixung_O(const Diem& A){
+    Diem C(0-A.getX(), 0-A.getY());
+    return C;
+}
+
+Diem fdoiXung_Ox(const Diem& A){
+    Diem C(A.getX(), 0-A.getY());
+    return C;
+}
+
+Diem fdoiXung_Oy(const Diem& A){
+    Diem C(0-A.getX(), A.getY());
+    return C;
+}
+
+bool Diem::KTtrungnhau(const Diem& A){
     return (x == A.x && y == A.y);
 }
+
+class TamGiac{
+    Diem A, B, C;
+    public:
+        TamGiac(){;}
+        TamGiac (Diem a, Diem b, Diem c) : A(a), B(b), C(c){}
+
+        double chuVi() {
+            return A.kCach(B) + B.kCach(C) + C.kCach(A);
+        }
+        // dien tich theo toa do 3 dinh
+        double dienTich(){
+            return abs((B.getX() - A.getX())*(C.getY() - A.getY())-(C.getX() - A.getX())*(B.getY() - A.getY()));
+        }
+        // kiem tra co phai tam giac khong
+        bool checkTG(){
+             double a = A.kCach(B);
+            double b = B.kCach(C);
+            double c = C.kCach(A);
+
+            if (a + b > c && a + c > b && c + b > a && a > 0 && b > 0 && c > 0)
+                return true;
+            return false;
+        }
+
+};
+
+
+class DanhSachTG{
+    int n;
+    Diem *p;
+    public:
+        DanhSachTG(){
+            n = 0;
+            p = nullptr;
+        }
+        DanhSachTG(int sl){
+            n = sl;
+            p = new Diem[n];
+        }
+        ~DanhSachTG(){
+            n = 0;
+            delete[] p;
+        }
+        double dienTich();
+        void Nhap();
+
+};
 
 int main(){
     
@@ -81,56 +175,6 @@ int main(){
     system("pause");
     return 0;
 }
-
-// class TamGiac{
-//     Diem A, B, C;
-//     public:
-//         TamGiac(){;}
-//         TamGiac (Diem a, Diem b, Diem c) : A(a), B(b), C(c){}
-
-//         double chuVi() {
-//             return A.kCach(B) + B.kCach(C) + C.kCach(A);
-//         }
-//         // dien tich theo toa do 3 dinh
-//         double dienTich(){
-//             return abs((B.x - A.x)*(C.y - A.y)-(C.x - A.x)*(B.y - A.y));
-//         }
-//         // kiem tra co phai tam giac khong
-//         bool checkTG(){
-//              double a = A.kCach(B);
-//             double b = B.kCach(C);
-//             double c = C.kCach(A);
-
-//             if (a + b > c && a + c > b && c + b > a && a > 0 && b > 0 && c > 0)
-//                 return true;
-//             return false;
-//         }
-
-
-// };
-
-
-// class DanhSachTG{
-//     int n;
-//     Diem *p;
-//     public:
-//         DanhSachTG(){
-//             n = 0;
-//             p = nullptr;
-//         }
-//         DanhSachTG(int sl){
-//             n = sl;
-//             p = new Diem[n];
-//         }
-//         ~DanhSachTG(){
-//             n = 0;
-//             delete[] p;
-//         }
-//         double dienTich()
-//         float 
-//         void Nhap();
-
-// };
 
 int checkVT(int x, int y){
     if (x > 0 && y > 0)
